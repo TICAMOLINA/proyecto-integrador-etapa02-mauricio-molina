70950fca6e946473c3be4bb45d07e97614212ad3
Mauricio Molina <tica.molina57@gmail.com>
2025-03-19 20:04:26 -0300

    Creo el archivo Rutas, utilizo react-router y agrego todas las paginas

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,11 +1,38 @@
 import { useRoutes } from "react-router"
+import Inicio from "../pages/Inicio"
+import Contacto from "../pages/Contacto"
+import Nosotros from "../pages/Nosotros"
+import Carrito from "../pages/Carrito"
+import Alta from "../pages/Alta"
+import NoEncontrado from "../pages/NoEncontrado"
 
 const Rutas = () => {
 
     const hookRutas = useRoutes(
         [
             {
-
+                path: '/',
+                element: <Inicio />
+            },
+            {
+                path: '/contacto',
+                element: <Contacto />
+            },
+            {
+                path: '/nosotros',
+                element: <Nosotros />
+            },
+            {
+                path: '/carrito',
+                element: <Carrito />
+            },
+            {
+                path: '/alta',
+                element: <Alta />
+            },
+            {
+                path: '*',
+                element: <NoEncontrado />
             }
         ]
     )
