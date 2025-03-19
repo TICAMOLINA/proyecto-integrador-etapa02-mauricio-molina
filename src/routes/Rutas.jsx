1e505a4e3334b334c2861c0f70a99a11c371904a
Mauricio Molina <tica.molina57@gmail.com>
2025-03-19 19:54:30 -0300

    Creo los componentes dentro de pages para las paginas: alta, carrito, contacto, inicio, noencontrado y nosotros

---
+++ src/routes/Rutas.jsx
@@ -0,0 +1,16 @@
+import { useRoutes } from "react-router"
+
+const Rutas = () => {
+
+    const hookRutas = useRoutes(
+        [
+            {
+
+            }
+        ]
+    )
+  
+    return hookRutas
+}
+
+export default Rutas
