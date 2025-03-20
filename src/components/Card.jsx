b1f26d50a87a8051311b1a407028598ce6085031
Mauricio Molina <tica.molina57@gmail.com>
2025-03-19 21:41:14 -0300

    Creo la carpeta data y en su interior el archivo db.json

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,12 @@ const Card = () => {
         <div className="card">
             <article className="card__article">
                 <div className="card__image-container">
-                    <img className="card__image" src="foto" alt="texto foto"/>
+                    <img className="card__image" src="/public/imgs/alemania-1990.webp" alt="texto foto"/>
                 </div>
                 <div className="card__content">
-                    <h2 className="card__heading">Nombre</h2>
+                    <h2 className="card__heading">Camiseta Alemania - Mundial Italia 1990</h2>
                     <div className="card__description">
-                        <p>Descripcion</p>
+                        <p>Camiseta Alemania - Mundial Italia 1990</p>
                     </div>
                 </div>
             </article>
