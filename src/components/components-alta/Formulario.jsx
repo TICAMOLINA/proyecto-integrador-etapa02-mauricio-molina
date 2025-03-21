1239331286df6e4c7e557ee2b94b9d12dc59f091
Mauricio Molina <tica.molina57@gmail.com>
2025-03-20 23:18:01 -0300

    Dentro de Formulario creo las funciones handleChange y handleReset. A la primera la uso como callback en el evento onChange dentro de los input y la restante que es un setForm a formInicial la agrego en un evento onClick dentro del boton de limpiar. Para handleChange desestructura del e.target el type, name, checked y velue para luego en el setForm tener todo lo que tiene el form, y evaluar si el type de name es checkbox, por lo que esto quiere decir que si estoy en un input que no es checkbox retorna false, por lo que almacena la value, y si lo estoy almacena el checked

--- src/components/components-alta/Formulario.jsx
+++ src/components/components-alta/Formulario.jsx
@@ -15,43 +15,77 @@ const Formulario = () => {
 
     const [form, setForm] = useState(formInicial)
 
+    const handleSubmit = () => {
+        
+    }
+
+    const handleChange = (e) => {
+
+        const { type, name, checked, value } = e.target
+        setForm({
+            ...form,
+            [name]: type === 'checkbox' ? checked : value
+        })
+    }
+
+    const handleReset = () => {
+        setForm(formInicial)
+    }
+
   return (
     <>
         
         <h2>Agregar : Editar</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="lbl-nombre">Nombre</label>
-                <input type="text" id="lbl-nombre" name="nombre" value={form.nombre}/>
+                <input onChange={handleChange}
+                type="text" id="lbl-nombre" 
+                name="nombre" 
+                value={form.nombre}/>
             </div>
             <div>
                 <label htmlFor="lbl-precio">Precio</label>
-                <input type="text" id="lbl-precio" name="precio" value={form.precio}/>
+                <input onChange={handleChange}
+                type="text" id="lbl-precio" 
+                name="precio" 
+                value={form.precio}/>
             </div>
             <div>
                 <label htmlFor="lbl-categoria">Categoría</label>
-                <input type="text" id="lbl-categoria" name="categoria" value={form.categoria}/>
+                <input onChange={handleChange} 
+                type="text" id="lbl-categoria" 
+                name="categoria" 
+                value={form.categoria}/>
             </div>
             <div>
                 <label htmlFor="lbl-descripcion">Descripción</label>
-                <input type="text" id="lbl-descripcion" name="descripcion" value={form.descripcion}/>
+                <input onChange={handleChange} 
+                type="text" id="lbl-descripcion" 
+                name="descripcion" value={form.descripcion}/>
             </div>
             <div>
                 <label htmlFor="lbl-stock">Stock</label>
-                <input type="text" id="lbl-stock" name="stock" value={form.stock}/>
+                <input onChange={handleChange} 
+                type="text" id="lbl-stock" 
+                name="stock" value={form.stock}/>
             </div>
             <div>
                 <label htmlFor="lbl-foto">Foto</label>
-                <input type="text" id="lbl-foto" name="foto" value={form.foto}/>
+                <input onChange={handleChange} 
+                type="text" id="lbl-foto" 
+                name="foto" value={form.foto}/>
             </div>
             <div>
                 <label htmlFor="lbl-envio">Envío</label>
-                <input type="checkbox" id="lbl-envio" name="envio" checked={form.envio}/>
+                <input onChange={handleChange}
+                type="checkbox" id="lbl-envio" 
+                name="envio" checked={form.envio}/>
             </div>
 
             <button type="submit">Guardar : Editar</button>
-            <button type="reset">Limpiar</button>
+            <button onClick={handleReset} type="reset">Limpiar</button>
         </form>
     </>
   )
