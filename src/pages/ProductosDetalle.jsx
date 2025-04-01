062eb71dbb76c8500cf87d735347564ecafc6f98
Mauricio Molina <tica.molina57@gmail.com>
2025-04-01 18:06:51 -0300

    Finalizo los estilos de la página de productos detalles, en todos las media queries

--- src/pages/ProductosDetalle.jsx
+++ src/pages/ProductosDetalle.jsx
@@ -1,62 +1,74 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router"
 import Spinner from "../utils/Spinner"
 import Slider from "../components/Slider"
 import ARSConvert from "../utils/ARSConvert"
 import './ProductosDetalle.scss'
+import CarritoContext from "../contexts/CarritoContext"
 
 const ProductosDetalle = () => {
 
-    const { id } = useParams()
+  const handleAgregar = (productoDetalle) => {
+    agregarProductoAlCarritoContext(productoDetalle)
+  }
 
-    const [productoDetalle, setProductoDetalle] = useState(null)
-    
-    useEffect(() => {
-      getOne(id)
-    }, [])
-   
-    const getOne = async (id) => {
-        const urlGetOne = import.meta.env.VITE_BACKEND_PRODUCTOS + id
+  const { id } = useParams()
 
-        try {
-            const res = await fetch(urlGetOne)
+  const { agregarProductoAlCarritoContext } = useContext(CarritoContext)
 
-            if (!res.ok) {
-                throw new Error('No se pudo obtener producto')
-            }
-            const data = await res.json()
-            setProductoDetalle(data)
 
-        } catch (error) {
-            console.error('[getOne]', error)
-        }
+  const [productoDetalle, setProductoDetalle] = useState(null)
+
+  useEffect(() => {
+    getOne(id)
+  }, [])
+
+  const getOne = async (id) => {
+    const urlGetOne = import.meta.env.VITE_BACKEND_PRODUCTOS + id
+
+    try {
+      const res = await fetch(urlGetOne)
+
+      if (!res.ok) {
+        throw new Error('No se pudo obtener producto')
+      }
+      const data = await res.json()
+      setProductoDetalle(data)
+
+    } catch (error) {
+      console.error('[getOne]', error)
     }
+  }
+
+
   return (
     <>
 
-    {
-      productoDetalle ?
-      (
-        <main className="product-detail">
-          <section className="product-detail__slider">
-            <Slider data={productoDetalle}/>
-          </section>
-          <section className="product-detail__text-content">
-            <h2>{productoDetalle.nombre}</h2>
-            <p>Categoría: <span>{productoDetalle.categoria}</span></p>
-            <p>{productoDetalle.descripcion}</p>
-            <p>Disponible por <span>{ARSConvert(productoDetalle.precio)}</span></p>
-            <div className="product-detail__btn-container">
-              <button className="product-detail__btn-add">AÑADIR AL CARRITO</button>
-            </div>
-          </section> 
-        </main>
+      {
+        productoDetalle ?
+          (
+            <main className="product-detail">
+              <section className="product-detail__slider">
+                <Slider data={productoDetalle} />
+              </section>
+              <section className="product-detail__text-content">
+                <h2>{productoDetalle.nombre}</h2>
+                <p>Categoría: <span>{productoDetalle.categoria}</span></p>
+                <p>{productoDetalle.descripcion}</p>
+                <p>Disponible por <span>{ARSConvert(productoDetalle.precio)}</span></p>
+                <div className="product-detail__btn-container">
+                  <button
+                    onClick={() => handleAgregar(productoDetalle)}
+                    className="product-detail__btn-add">AÑADIR AL CARRITO</button>
+                </div>
+              </section>
+            </main>
 
-      ) :
-      (
-       <Spinner />
-      )
-    }
+          ) :
+          (
+            <Spinner />
+          )
+      }
 
     </>
 
